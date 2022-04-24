f59d9bba2dafa6c064b0d95ebfbb5843fe1d5c6d
varenyeolad <97789571+varenyeolad@users.noreply.github.com>
2022-04-24 21:19:42 +0600

    Added ALL

---
+++ java.js
@@ -0,0 +1,35 @@
+// Imports
+const express = require('express')
+const app = express()
+const port = 3000
+const https = require('https');
+
+let bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.set('view-engine', 'ejs')
+app.get('/', function(req, res) {
+    res.render('index.ejs');
+});
+app.get('/aff', function(req, res) {
+    res.render('Affirmation.ejs');
+});
+app.get('/med', function(req, res) {
+    res.render('Meditation.ejs');
+});
+app.get('/ff', function(req, res) {
+    res.render('greet.ejs');
+});
+app.get('')
+// Load CSS
+app.use(express.static('public'));
+
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js')) //if we'll add
+app.use('/img', express.static(__dirname + 'public/images'))
+
+
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
